d0d40d54dc837ec4bba76bc0a87c2179a49dddbb
MikaMacro <mneukirch@stud.macromedia.de>
2024-11-05 10:46:38 +0100

    finished uebung

--- Lightning/Assets/Scripts/Button Logic.cs
+++ Lightning/Assets/Scripts/Button Logic.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
+
+//Implementieren von den Methoden (Save, Load, Delete (cleart UI und PlayerPrefs)) mit Playerprefs + verknüpfen
+// Set = save, Get = load, Delete = löschen
+//
 
 public class ButtonLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_InputField inputField;
+    public void SaveData()
     {
+        
+        PlayerPrefs.SetString("Input", inputField.text);
         
     }
-
-    // Update is called once per frame
-    void Update()
+    public void LoadData()
     {
-        
+        PlayerPrefs.GetString("Input");
+    }
+    public void DeleteData()
+    {
+        PlayerPrefs.DeleteKey("Input");
+        inputField.text = string.Empty;
+        //PlayerPrefs.DeleteAll();
     }
 }
+
+
+
+
+
