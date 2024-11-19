1bcab150019b9b803428a0da95f9bdd09011447d
MikaMacro <mneukirch@stud.macromedia.de>
2024-11-19 09:40:04 +0100

    Color Save dazu

--- Lightning/Assets/Scripts/ButtonPosition.cs
+++ Lightning/Assets/Scripts/ButtonPosition.cs
@@ -12,13 +12,25 @@ public class ButtonPosition : MonoBehaviour
         PlayerPrefs.SetFloat("PositionX", player.transform.position.x); 
         PlayerPrefs.SetFloat("PositionY", player.transform.position.y); //positionen
         PlayerPrefs.SetFloat("PositionZ", player.transform.position.z);
+        
     }
     public void Load() // läd  Button
     {
-        Vector3 XYZ = new Vector3(PlayerPrefs.GetFloat("PositionX"), PlayerPrefs.GetFloat("PositionY"), PlayerPrefs.GetFloat("PositionZ"));
+        //Vector3 XYZ = new Vector3(PlayerPrefs.GetFloat("PositionX"), PlayerPrefs.GetFloat("PositionY"), PlayerPrefs.GetFloat("PositionZ"));
         //Auf Vector 3 ist (name) XYZ genauso wie, eine float für X Y und Z 
-        player.transform.position = XYZ; //sagt, das es für die XYZ achse gilt
-        print(PlayerPrefs.GetString("Positionbekommen"));
+        //player.transform.position = XYZ; //sagt, das es für die XYZ achse gilt
+
+        if (PlayerPrefs.HasKey("player.transform.position.x"))
+        {
+            Vector3 XYZ = new Vector3(PlayerPrefs.GetFloat("PositionX"), PlayerPrefs.GetFloat("PositionY"), PlayerPrefs.GetFloat("PositionZ"));
+            player.transform.position = XYZ;
+            
+        }
+        else
+        {
+           print("NoPositionSaved");
+        }
+        
     }
     
 }
---
+++ Lightning/Assets/Scripts/ColorSave.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ColorSave : MonoBehaviour
+{
+    
+    public void Save()
+    {
+
+    }
+    public void Load()
+    {
+
+    }
+}
