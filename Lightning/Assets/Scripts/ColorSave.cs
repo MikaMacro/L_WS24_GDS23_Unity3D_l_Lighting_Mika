81671bdcc7aefcd53268c2e4bba5991c20588bc8
MikaMacro <mneukirch@stud.macromedia.de>
2024-11-19 11:24:58 +0100

    Color done

--- Lightning/Assets/Scripts/ColorSave.cs
+++ Lightning/Assets/Scripts/ColorSave.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+//using Unity.VisualScripting;
 using UnityEngine;
 
 public class ColorSave : MonoBehaviour
 {
-    
+    private Color color;
+    private string colorString;
+
     public void Save()
     {
-
+        color = GetComponent<MeshRenderer>().material.color;
+        colorString = ColorUtility.ToHtmlStringRGBA(color);
+        PlayerPrefs.SetString("colorString", colorString);
+        PlayerPrefs.Save();
     }
     public void Load()
     {
+        colorString = PlayerPrefs.GetString("colorString");
+        
 
+        if (ColorUtility.TryParseHtmlString("#" + colorString, out color))
+        {
+            GetComponent<MeshRenderer>().material.color = color;
+        }
+        else
+        {
+            print("Error to set color:");
+        }
+       
     }
 }
