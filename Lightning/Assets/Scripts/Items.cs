b0e4bdc23f9dcb1c4061380635f4ab70eb10c7be
MikaMacro <mneukirch@stud.macromedia.de>
2024-12-03 10:31:51 +0100

    New Uebung

--- Lightning/Assets/Scripts/Items.cs
+++ Lightning/Assets/Scripts/Items.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Items : MonoBehaviour
 {
+    private AudioSource explodison;
+    private void Awake()
+    {
+        explodison = GetComponent<AudioSource>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Item"))
         {
           Destroy(collision.gameObject);
+        explodison.Play();
         } 
     }
 }
